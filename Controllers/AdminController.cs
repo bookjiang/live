37627b3da0d6e70c1ec428fcde83981b0c5f4e9c
爽 柳 <2774851070@qq.com>
2021-01-15 11:19:32 +0800

    删除用户

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Microsoft.EntityFrameworkCore;
 
 namespace live.Controllers
 {
@@ -20,13 +20,44 @@ namespace live.Controllers
             _context = context;
         }
 
-       
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.id == id);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Admin>>> GetUsers()
+        {
+            return await _context.Admins.ToListAsync();
+        }
 
         [HttpGet("{id}")]
         public ActionResult<Admin> Get(int id)
         {
             return _context.Admins.Find(id);
         }
+
+        //api/Admin/3
+        [HttpDelete("{userId}")]
+        public async Task<ActionResult> DeleteUserByuserId(int userId)
+        {
+            var user =await _context.Users.FindAsync(userId);
+            ResultState resultState = new ResultState();
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            if (!UserExists(user.id))
+            {
+                resultState.success = true;
+                resultState.code = 1;
+                resultState.message = "成功删除";
+                return new JsonResult(resultState);
+            }
+            resultState.message = "删除失败";
+            return new JsonResult(resultState);
+        }
+
+
+
 
         [HttpPost("login")]
         public JsonResult login(Admin admin)
@@ -50,18 +81,12 @@ namespace live.Controllers
             else
             {
                 resultState.success = false;
-                resultState.message = "密码错误";
+                resultState.message = "用户名或密码错误";
                 return new JsonResult(resultState);
             }
 
 
-
         }
-
-
-
-
-
 
         public bool UserNameExists(string name)
         {
