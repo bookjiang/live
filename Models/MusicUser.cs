5c038fd9ff1c6d29277e57ff014223d2e1afa92b
爽 柳 <2774851070@qq.com>
2021-01-21 17:16:03 +0800

    新建MusicUser实体

---
+++ Models/MusicUser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace live.Models
+{
+    /// <summary>
+    /// 音乐 user类
+    /// </summary>
+    public class MusicUser
+    {
+        /// <summary>
+        /// 构造
+        /// </summary>
+        public MusicUser()
+        {
+
+        }
+
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string tel { get; set; }
+        public string id_no { get; set; }
+        public string psd { get; set; }
+        public int role { get; set; }//0：管理员 1：用户
+    }
+}
